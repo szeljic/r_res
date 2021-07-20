ebcb608009285642e3387c7366e7d62f2dacfa40
Đorđe Zeljić <zeljic@gmail.com>
2021-07-20 23:59:27 +0200

    Init and cleaned ui project

---
+++ ui/src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '@/views/Home';
+
+Vue.use(VueRouter)
+
+const routes = [
+	{
+		path: '/',
+		component: Home
+	}
+]
+
+const router = new VueRouter({
+	routes
+})
+
+export default router
