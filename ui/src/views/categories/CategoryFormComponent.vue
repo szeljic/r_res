02ccb4e07f915a64a1c66a8cddc1bdcdbe8c8488
Đorđe Zeljić <zeljic@gmail.com>
2021-07-28 23:25:40 +0200

    Handle categories

--- ui/src/views/categories/CategoryFormComponent.vue
+++ ui/src/views/categories/CategoryFormComponent.vue
@@ -1,24 +1,29 @@
 <template>
-	<v-form ref="frm" @submit.prevent="submit" v-model="valid">
-		<v-card>
+	<v-form ref="frm" @submit.prevent="submit" v-model="valid" :disabled="loading">
+		<v-card elevation="3">
 			<v-card-title>Dodaj</v-card-title>
+			<v-divider></v-divider>
 			<v-card-text>
 				<v-text-field
 					outlined
 					v-model="item.name"
 					label="Ime"
+					:rules="[$v.required]"
 				></v-text-field>
 				<v-textarea
 					outlined
 					v-model="item.description"
 					label="Opis"
+					:rules="[$v.required]"
+					counter
 				></v-textarea>
-				<v-divider></v-divider>
 			</v-card-text>
+			<v-divider></v-divider>
 			<v-card-actions>
 				<v-spacer></v-spacer>
 				<v-btn color="primary" :disabled="disabled || !valid" type="submit">Potvrdi</v-btn>
 				<v-btn :disabled="disabled" @click.prevent="reset">Očisti</v-btn>
+				<v-btn :disabled="disabled" @click.prevent="$emit('close')">Zatvori</v-btn>
 			</v-card-actions>
 		</v-card>
 	</v-form>
@@ -31,15 +36,25 @@
 		{
 			return {
 				disabled: false,
+				loading: false,
 				valid: false,
 				item: {
-					name: null
+					name: null,
+					description: null
 				}
 			};
 		},
 		props: {
 			id: {
-				type: Number
+				type: Number,
+				default: null
+			}
+		},
+		created()
+		{
+			if (this.id !== null)
+			{
+				this.fetch();
 			}
 		},
 		methods: {
@@ -50,18 +65,53 @@
 			},
 			async submit()
 			{
+				const me = this;
+
 				if (!this.$refs.frm.validate())
 				{
 					return;
 				}
 
-				const response = await this.$http({
-					url: '/api/v1/categories',
-					data: this.item,
-					method: 'POST'
-				});
+				this.loading = true;
 
-				console.log(response);
+				try
+				{
+					const {data} = await this.$http({
+						url: '/api/v1/categories' + (this.id ? '/' + this.id : ''),
+						data: {
+							name: this.item.name,
+							description: this.item.description
+						},
+						method: this.id ? 'PATCH' : 'POST'
+					});
+
+					me.$emit('success', data);
+				} catch (e)
+				{
+					me.$emit('failed', e);
+				} finally
+				{
+					this.loading = false;
+				}
+			},
+			async fetch()
+			{
+				this.loading = true;
+
+				try
+				{
+					const {data} = await this.$http({
+						url: `/api/v1/categories/${this.id}`
+					});
+
+					Object.keys(this.item).forEach(k => this.item[k] = data[k]);
+				} catch (e)
+				{
+					this.$emit('failed', e);
+				} finally
+				{
+					this.loading = false;
+				}
 			}
 		}
 	};
